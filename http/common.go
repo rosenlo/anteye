9ee38ba4cdedc0a5855fa9a797d37a0f2c0094f2
罗子豪 <rosenluov@gmail.com>
2018-09-28 19:39:15 +0800

    [REV] add an api that can reload the configuration
    Signed-off-by: 罗子豪 <rosenluov@gmail.com>

--- http/common.go
+++ http/common.go
@@ -3,6 +3,7 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/toolkits/file"
 
@@ -24,5 +25,13 @@ func configCommonRoutes() {
 
 	http.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
 		RenderDataJson(w, g.Config())
+	})
+	http.HandleFunc("/config/reload", func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.RemoteAddr, "127.0.0.1") {
+			g.ParseConfig(g.ConfigFile)
+			RenderDataJson(w, "ok")
+		} else {
+			RenderDataJson(w, "no privilege")
+		}
 	})
 }
